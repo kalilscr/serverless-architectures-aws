411195edfb0d41386ea627ccd41e7ee31b1f432b
Rodrigo Kalil <rkbuarque@gmail.com>
2024-11-27 17:09:24 -0300

    feat: react project with aws amplify

---
+++ react-amplified/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "reactamplifiedb73d0b88b73d0b88": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
